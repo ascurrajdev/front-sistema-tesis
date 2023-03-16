537dda04ed67cf1c89ff6acbb83eeb4d3904aeeb
Jose Ascurra <joseascurra123@gmail.com>
2023-03-16 00:36:49 -0300

    Agregado del formulario de edicion y creacion de usuarios

--- src/views/guards/users/users/CreateUsersView.vue
+++ src/views/guards/users/users/CreateUsersView.vue
@@ -1,14 +1,17 @@
 <script setup>
-import { reactive } from 'vue';
-import { useRouter } from 'vue-router';
+import { reactive, onMounted, watch, ref } from 'vue';
+import { useRouter, useRoute } from 'vue-router';
 import apiUsers from '@/services/apiUsers'
+import { notification } from 'ant-design-vue';
 import {useQuery} from '@tanstack/vue-query'
 import { useAuthUserStore } from '@/stores/users/authUser';
 const router = useRouter()
+const route = useRoute()
 const authUserStore = useAuthUserStore()
 const onBackHeader = () => {
     router.back()
 }
+const isSubmit = ref(false)
 const getAllRoles = async () => {
     const {data} = await apiUsers.get('roles',{
         headers:{
@@ -21,18 +24,38 @@ const {data} = useQuery({
     queryKey:['role_users'],
     queryFn:getAllRoles
 })
+
 const form = reactive({
     name:'',
     email:'',
     role_id:'',
     password:'',
-    password_confirmation:''
 })
+
+const onSubmit = () => {
+    isSubmit.value = true
+    apiUsers.post("",form,{
+        headers:{
+            'Authorization':`Bearer ${authUserStore.auth.credentials.plainTextToken}`
+        }
+    }).then(() => {
+        notification['success']({
+            message:'El usuario se ha guardado correctamente'
+        })
+        router.push('/guards/users/users')
+    }).catch(() => {
+        notification['error']({
+            message:'Hubo un error al guardar el usuario'
+        })
+    }).finally(() => {
+        isSubmit.value = false
+    })
+}
 </script>
 <template>
     <a-page-header title="Create User" @back="onBackHeader"/>
     <a-card>
-        <a-form layout="vertical" :model="form" autocomplete="off">
+        <a-form @finish="onSubmit" layout="vertical" :model="form" autocomplete="off">
             <a-form-item name="name" label="Name">
                 <a-input v-model:value="form.name"/>
             </a-form-item>
@@ -42,17 +65,14 @@ const form = reactive({
             <a-form-item name="password" label="Password">
                 <a-input type="password" v-model:value="form.password"/>
             </a-form-item>
-            <a-form-item name="password_confirmation" label="Password Confirmed">
-                <a-input type="password" v-model:value="form.password_confirmation"/>
-            </a-form-item>
             <a-form-item name="role_id" label="Role">
                 <a-select v-model:value="form.role_id">
-                    <a-select-option value="" disabled>N/A</a-select-option>
+                    <a-select-option :value="null" disabled>N/A</a-select-option>
                     <a-select-option v-for="role in data?.data" :value="role.id">{{ role.name }}</a-select-option>
                 </a-select>
             </a-form-item>
             <a-form-item>
-                <a-button type="primary" html-type="submit">Guardar</a-button>
+                <a-button type="primary" :loading="isSubmit" html-type="submit">Guardar</a-button>
             </a-form-item>
         </a-form>
     </a-card>
--- src/views/guards/users/users/EditUsersView.vue
+++ src/views/guards/users/users/EditUsersView.vue
@@ -1,7 +1,8 @@
 <script setup>
-import { reactive, onMounted, watch } from 'vue';
+import { reactive, onMounted, watch, ref } from 'vue';
 import { useRouter, useRoute } from 'vue-router';
 import apiUsers from '@/services/apiUsers'
+import { notification } from 'ant-design-vue';
 import {useQuery} from '@tanstack/vue-query'
 import { useAuthUserStore } from '@/stores/users/authUser';
 const router = useRouter()
@@ -10,6 +11,7 @@ const authUserStore = useAuthUserStore()
 const onBackHeader = () => {
     router.back()
 }
+const isSubmit = ref(false)
 const getAllRoles = async () => {
     const {data} = await apiUsers.get('roles',{
         headers:{
@@ -39,7 +41,6 @@ const form = reactive({
     email:'',
     role_id:'',
     password:'',
-    password_confirmation:''
 })
 onMounted(() => {
     if(!!userData.value){
@@ -55,11 +56,40 @@ watch(userData,(value) => {
         })
     }
 })
+const onSubmit = () => {
+    let formData = form
+    if(formData.password == ''){
+        delete formData.password
+    }
+    if(!!formData?.id){
+        delete formData.id
+    }
+    if(!!formData?.created_at){
+        delete formData.created_at
+    }
+    isSubmit.value = true
+    apiUsers.put(`${route.params.id}`,form,{
+        headers:{
+            'Authorization':`Bearer ${authUserStore.auth.credentials.plainTextToken}`
+        }
+    }).then(() => {
+        notification['success']({
+            message:'El usuario se ha guardado correctamente'
+        })
+        router.push('/guards/users/users')
+    }).catch(() => {
+        notification['error']({
+            message:'Hubo un error al guardar el usuario'
+        })
+    }).finally(() => {
+        isSubmit.value = false
+    })
+}
 </script>
 <template>
     <a-page-header title="Edit User" @back="onBackHeader"/>
     <a-card>
-        <a-form layout="vertical" :model="form" autocomplete="off">
+        <a-form @finish="onSubmit" layout="vertical" :model="form" autocomplete="off">
             <a-form-item name="name" label="Name">
                 <a-input v-model:value="form.name"/>
             </a-form-item>
@@ -69,17 +99,14 @@ watch(userData,(value) => {
             <a-form-item name="password" label="Password">
                 <a-input type="password" v-model:value="form.password"/>
             </a-form-item>
-            <a-form-item name="password_confirmation" label="Password Confirmed">
-                <a-input type="password" v-model:value="form.password_confirmation"/>
-            </a-form-item>
             <a-form-item name="role_id" label="Role">
                 <a-select v-model:value="form.role_id">
-                    <a-select-option value="" disabled>N/A</a-select-option>
+                    <a-select-option :value="null" disabled>N/A</a-select-option>
                     <a-select-option v-for="role in data?.data" :value="role.id">{{ role.name }}</a-select-option>
                 </a-select>
             </a-form-item>
             <a-form-item>
-                <a-button type="primary" html-type="submit">Guardar</a-button>
+                <a-button type="primary" :loading="isSubmit" html-type="submit">Guardar</a-button>
             </a-form-item>
         </a-form>
     </a-card>
